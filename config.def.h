d2e1f5acf5b9a868a68e710c28fcdaa70857c8b0
hexbitCTF <hexabit777@gmail.com>
2026-05-07 21:37:20 +0000

    Auto-sync: Thu May  7 21:37:20 UTC 2026

--- config.def.h
+++ config.def.h
@@ -102,38 +102,31 @@ float alpha = 0.8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-        /* 8 normal colors */
- [0]  = "#191970", /* black (Midnight Blue) */
- [1]  = "#ff5555", /* red */
- [2]  = "#50fa7b", /* green */
- [3]  = "#f1fa8c", /* yellow */
- [4]  = "#00FFFF", /* blue (Electric Blue) */
- [5]  = "#ff79c6", /* magenta */
- [6]  = "#87CEEB", /* cyan (Sky Blue) */
- [7]  = "#D3D3D3", /* white (Light Gray) */
+    /* 8 normal colors */
+    [0] = "#15161e", /* black */
+    [1] = "#f7768e", /* red */
+    [2] = "#9ece6a", /* green */
+    [3] = "#e0af68", /* yellow */
+    [4] = "#7aa2f7", /* blue */
+    [5] = "#bb9af7", /* magenta */
+    [6] = "#7dcfff", /* cyan */
+    [7] = "#a9b1d6", /* white */
 
-     /* 8 bright colors */
- [8]  = "#2A3D66", /* black (Deep Blue for dimmed elements) */
- [9]  = "#ff5555", /* red */
- [10] = "#50fa7b", /* green */
- [11] = "#f1fa8c", /* yellow */
- [12] = "#00FFFF", /* blue (Electric Blue) */
- [13] = "#ff79c6", /* magenta */
- [14] = "#87CEEB", /* cyan (Sky Blue) */
- [15] = "#ffffff", /* white (Light Gray) */
+    /* 8 bright colors */
+    [8]  = "#414868", /* black (bright) */
+    [9]  = "#f7768e", /* red (bright) */
+    [10] = "#9ece6a", /* green (bright) */
+    [11] = "#e0af68", /* yellow (bright) */
+    [12] = "#7aa2f7", /* blue (bright) */
+    [13] = "#bb9af7", /* magenta (bright) */
+    [14] = "#7dcfff", /* cyan (bright) */
+    [15] = "#c0caf5", /* white (bright) */
 
-     /* special colors */
- [256] = "#1e1e1e", /* background (Dark Gray for background) */
- [257] = "#D3D3D3", /* foreground (Light Gray for text) */
-
-        /* more colors can be added after 255 to use with DefaultXX */
- "#cccccc", /* soft gray for inactive elements */
- "#555555", /* muted dark gray for borders */
- "gray90", /* default foreground colour */
- "black", /* default background colour */
+    /* special colors */
+    [256] = "#24283b", /* background (Storm) */
+    [257] = "#c0caf5", /* foreground */
+    [258] = "#3b4261", /* selection background */
 };
-
-
 
 /*
  * Default colors (colorname index)
@@ -142,7 +135,8 @@ static const char *colorname[] = {
 unsigned int defaultfg = 257;
 unsigned int defaultbg = 256;
 unsigned int defaultcs = 257;
-static unsigned int defaultrcs = 257;
+static unsigned int defaultrcs = 256;
+
 
 /*
  * Default shape of cursor
